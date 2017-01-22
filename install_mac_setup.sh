094533f7ff9e67a478974524c5d76a2fd51676b7
killroy1971 <glennsnead@gmail.com>
2017-01-22 00:35:18 -0500

    Changes to be committed: 	modified:   homebrew-packages.yml 	modified:   install_mac_setup.sh
    Added logic checking for XCode CLI package to install_mac_setup.sh
    Changed name of Python task to Python 2 task - homebrew-packages.yml

--- install_mac_setup.sh
+++ install_mac_setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [ ! -x "/usr/bin/make" ]; then
+  if [ -x "/usr/bin/xcode-select" ]; then /usr/bin/xcode-select --install; fi
+else
+  echo "Homebrew requied the Xcode Command Line Tools (CLT) to install."
+  echo "Please install this package and run this script again."
+  exit 1
+fi
+
+if [ ! -x "/usr/local/bin/brew" ]; then 
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # Update homebrew
 brew update
